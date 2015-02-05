fdcdd8c3dbd2258c5aca5b204712f87369d92e6f
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-05 15:20:23 +0100

    create a prototype for a train

--- JS1k.js
+++ JS1k.js
@@ -1,3 +1,22 @@
-// THIS IS WHERE YOUR DEMO GOES
-console.log('demo here', window.a, window.b, window.c, window.g);
-// END
+;(function(body, canvas, context){
+    var Point = function(x, y){
+	this.x = x;
+	this.y = y;
+    };
+
+    var Train = function(startPosition){
+	this.position =  startPosition || new Point(0, 0);
+    };
+
+    var TrainView = function(model, canvas, context){
+	this.model = model;
+	this.context = context;
+	this.update();
+    };
+    TrainView.prototype.update = function(){
+	this.context.fillRect(this.model.position.x, this.model.position.y, 5, 5);
+    };
+
+    var train = new Train(new Point(50, 50));
+    new TrainView(train, canvas, context);
+})(window.b, window.a, window.c);
